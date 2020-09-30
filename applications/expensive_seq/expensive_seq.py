b76069add79c251586c08c2aab7db4009437a710
Jason Clegg <tjason.clegg@gmail.com>
2020-09-30 16:08:07 -0600

    Submitting for day 3 MVP

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,9 +1,24 @@
 # Your code here
 
+cache = {}
+
 
 def expensive_seq(x, y, z):
     # Your code here
 
+    # Check if x, y, and z are in the cache
+    if(x, y, z) not in cache:
+        if x <= 0:
+            return y + z
+        if x > 0:
+            cache[(x, y, z)] = expensive_seq(x - 1, y + 1, z) + \
+                expensive_seq(x - 2, y + 2, z * 2) + \
+                expensive_seq(x - 3, y + 3, z * 3)
+
+        return cache[(x, y, z)]
+
+    else:
+        return cache[(x, y, z)]
 
 
 if __name__ == "__main__":
